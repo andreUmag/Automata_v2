b4946191b1ec0253df73c4d35d07505eaed8b091
andreUmag <142335765+andreUmag@users.noreply.github.com>
2023-10-02 17:40:49 -0500

    acomodar

--- templates/principal/acrtions.js
+++ templates/principal/acrtions.js
@@ -16,12 +16,12 @@ listelement.forEach((listelement) => {
 
 const slideValue = document.querySelector("span");
 const inputSlider = document.querySelector("input");
-inputSlider.oninput = (()=>{
+inputSlider.oninput = () => {
   let value = inputSlider.value;
   slideValue.textContent = value;
-  slideValue.style.left = (value) + "%";
+  slideValue.style.left = value + "%";
   slideValue.classList.add("show");
-});
-inputSlider.onblur = (()=>{
+};
+inputSlider.onblur = () => {
   slideValue.classList.remove("show");
-});
+};
