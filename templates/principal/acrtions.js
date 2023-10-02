6f723e85483e95791100cd128151b36031e8b9a9
andreUmag <142335765+andreUmag@users.noreply.github.com>
2023-10-02 17:36:14 -0500

    linea de velocidad terminada

--- templates/principal/acrtions.js
+++ templates/principal/acrtions.js
@@ -13,3 +13,15 @@ listelement.forEach((listelement) => {
     menu.style.height = `${height}px`;
   });
 });
+
+const slideValue = document.querySelector("span");
+const inputSlider = document.querySelector("input");
+inputSlider.oninput = (()=>{
+  let value = inputSlider.value;
+  slideValue.textContent = value;
+  slideValue.style.left = (value) + "%";
+  slideValue.classList.add("show");
+});
+inputSlider.onblur = (()=>{
+  slideValue.classList.remove("show");
+});
