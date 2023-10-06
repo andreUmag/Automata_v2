92d32804062cc5289db95234f781daf0101bbc0a
andreUmag <142335765+andreUmag@users.noreply.github.com>
2023-10-06 13:06:22 -0500

    cambios en carpetas

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, template_rendered
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world(): 
+    return template_rendered('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)
