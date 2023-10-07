0241a0f441b78b3407411c3178e01cc49520f32f
andreUmag <142335765+andreUmag@users.noreply.github.com>
2023-10-07 00:04:17 -0500

    cambio de idiomas

---
+++ app/static/scripts/translations.js
@@ -0,0 +1,50 @@
+const translations = {
+    historial: {
+      espanol: "Historial",
+      ingles: "History",
+    },
+    idiomas: {
+      espanol: "Idiomas",
+      ingles: "Languages",
+    },
+    espanol: {
+      espanol: "Español",
+      ingles: "Spanish",
+    },
+    ingles: {
+      espanol: "Inglés",
+      ingles: "English",
+    },
+    titulo: {
+      espanol: "Automata Finito",
+      ingles: "Finite Automaton",
+    },
+    palabra: {
+      espanol: "Palabra",
+      ingles: "Word",
+    },
+  };
+
+  function changeLanguage(lang) {
+    const elementsToTranslate = document.querySelectorAll('[data-translate]');
+    
+    elementsToTranslate.forEach((element) => {
+      const translationKey = element.getAttribute('data-translate');
+      const translation = translations[translationKey][lang];
+      if (translation) {
+        element.textContent = translation;
+      }
+    });
+  }
+  
+  //cambio de idioma cuando se hace clic en los enlaces de idioma
+  document.querySelectorAll('.nav_link--inside').forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const lang = link.getAttribute('data-translate');
+      changeLanguage(lang);
+    });
+  });
+  
+  changeLanguage('espanol');
+  
