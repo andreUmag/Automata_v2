39a23beb4c4fcf264e1e65abc7e1426262a3e935
andreUmag <142335765+andreUmag@users.noreply.github.com>
2023-10-06 21:42:22 -0500

    input funcional

--- app/static/scripts/acrtions.js
+++ app/static/scripts/acrtions.js
@@ -25,3 +25,31 @@ inputSlider.oninput = () => {
 inputSlider.onblur = () => {
   slideValue.classList.remove("show");
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+  const inputField = document.getElementById("input-word");
+  const addButton = document.getElementById("add-word-button");
+  const wordList = document.getElementById("word-list");
+
+  console.log(inputField); // Verifica si inputField es null
+  console.log(addButton); // Verifica si addButton es null
+  console.log(wordList);  // Verifica si wordList es null
+
+  addButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const inputValue = inputField.value.trim();
+
+    if (inputValue !== "") {
+      const listItem = document.createElement("li");
+      listItem.textContent = inputValue;
+
+      wordList.appendChild(listItem);
+
+      inputField.value = "";
+    }
+  });
+});
+
+
+
