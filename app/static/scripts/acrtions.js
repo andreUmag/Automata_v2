048c24bad0e35b7ab84e9978603650e5ba625f7a
Mauricio Polo <mauriciopoloem@gmail.com>
2023-10-09 23:43:24 -0500

    Finalizacion de la animacion

--- app/static/scripts/acrtions.js
+++ app/static/scripts/acrtions.js
@@ -15,21 +15,19 @@ listelement.forEach((listelement) => {
   });
 });
 
-// Section for the speed slider functionality
-const slideValue = document.querySelector("span");
-const inputSlider = document.querySelector("input");
-var valorInput = "";
+  // // Section for the speed slider functionality
+  // const slideValue = document.querySelector("span");
+  // const inputSlider = document.querySelector("input");
 
-function transformValorInput(value) {
-  result = (value - 10) / 90 * 1750 + 250
-}
-inputSlider.oninput = () => {
-  let value = inputSlider.value;
-  slideValue.textContent = value;
-  slideValue.style.left = value + "%";
-  slideValue.classList.add("show");
-  valorInput = transformarValorInput(value);
-};
+// inputSlider.oninput = () => {
+//   let value = inputSlider.value;
+//   slideValue.textContent = value;
+//   slideValue.style.left = value + "%";
+//   slideValue.classList.add("show");
+//   let speed = transformValorInput(value);
+  
+  
+// };
 inputSlider.onblur = () => {
   slideValue.classList.remove("show");
 };
@@ -43,10 +41,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   addButton.addEventListener("click", function (event) {
     event.preventDefault();
-    const inputValue = inputField.value.trim();
-    const regex = /^[abAB]+$/;
+    var text = inputField.value.trim();
+    inputField.value = "";
 
-    if (regex.test(inputValue)) {
+    if (check_text_with_regex(text) && text != "") {
       const listItem = document.createElement("li");
       listItem.textContent = inputValue;
 
@@ -55,91 +53,132 @@ document.addEventListener("DOMContentLoaded", function () {
       }
 
       wordList.insertBefore(listItem, wordList.firstChild);
-      inputField.value = "";
+
     } else {
-      alert("La palabra solo debe contener las letras 'a' y 'b'.");
-      speak("Cadena escrita incorrectamente, la palabra solo debe contener las letras 'a' y 'b'.")
+      inputField.value = "";
     }
   });
 });
 
 
 function check(){
-
-  var text = document.getElementById("input-word").value.toLowerCase();
+    
+    var text = document.getElementById("input-word").value.toLowerCase();
+    var value_slider = document.getElementById("inputSlider").value;
+    var speed = transformValorInput(value_slider);
     unpaint();
-    check_word(text, 0, -1);
-
-    function check_word(text, index, keynode) {
-    node= myDiagram.findNodeForKey(keynode);
-    window.setTimeout(function () {
-      paint_node(node);
-      window.setTimeout(function () {
-        if(index<text.length) {
-          let links = node.findTreeChildrenLinks();
-          let link = links.ub._dataArray.filter(function (link) {return link.data.text == text[index] && link.fromNode == node;});
-          if(link.length == 0) {
-          unpaint(myDiagram);
-            check_acceptance_status(node);
-          } else if(link[0].data.text == text[index]) {
-            unpaint_node(node);
-            paint_link(link[0]);
-            return check_word(text, index+1, link[0].toNode.data.id);
-          }
-        } else {
-          check_acceptance_status(node);
-        }
-      }, 500);
-    },500);
-  }
-  
-  function unpaint() {
-    myDiagram.model = go.Model.fromJson(document.getElementById("mySavedModel").value);
-  }
-  
-  
-  function paint_node(node) {
-    var shape = node.findObject("SHAPE");
-    shape.fill = "#A5D7F4";
-  }
-  
-  function unpaint_node(node) {
-    var shape = node.findObject("SHAPE");
-    shape.fill = "white";
-  }
-  
-  function paint_link(link) {
-    link.path.stroke = "#A5D7F4";
-    var shape = link.findObject("arrow");
-    shape.fill = "#7BCAF8";
-    window.setTimeout(function(){ 
-      link.path.stroke = "black";
-      shape.fill = "black";
-    },1000);
-  }
-  
-  function check_acceptance_status(node) {
-    if (text.match(/^[abAB]+$/)) {
-        if(node.data.category=="accept") {
-        alert("Cadena aceptada");
-        speak("La palabra ha sido aceptada");
-        return true;
-        } else {
-        alert("Cadena rechazada");
-        speak("La palabra ha sido rechazada");
-        return false;
-        }
+    if (!check_text_emptying(text)){
+      if (check_text_with_regex(text)){
+        check_word(text, 0, -1, speed);
+      } else {
+        alert("La palabra solo debe contener las letras 'a' y 'b'.");
+        speak("la palabra solo debe contener las letras a y b")
+      }
+    } else {
+      alert("No se ha ingresado ninguna cadena de texto.");
+      speak("No se ha ingresado ninguna cadena de texto.")
     }
+    
+}
+
+function check_word(text, index, keynode, speed) {
+  node= myDiagram.findNodeForKey(keynode);
+  window.setTimeout(function () {
+    paint_node(node);
+    window.setTimeout(function () {
+      if(index<text.length) {
+        let links = node.findTreeChildrenLinks();
+        let link = links.ub._dataArray.filter(function (link) {return link.data.text == text[index] && link.fromNode == node;});
+        if(link.length == 0) {
+          unpaint(myDiagram);
+          check_acceptance_status(node);
+        } else if(link[0].data.text == text[index]) {
+          unpaint_node(node);
+          paint_link(link[0], speed);
+          return check_word(text, index+1, link[0].toNode.data.id, speed);
+        }
+      } else {
+        check_acceptance_status(node);
+      }
+    }, speed);
+  },speed/2);
+}
+
+function unpaint() {
+  myDiagram.model = go.Model.fromJson(document.getElementById("mySavedModel").value);
+}
+
+function check_text_with_regex(text) {
+  if (text.match(/^[abAB]+$/)) {
+    return true;
+  } else {
+    return false;
   }
 }
 
-function speak(text) {
-  if ('speechSynthesis' in window) {
-    const speech = new SpeechSynthesisUtterance();
-    speech.text = text;
-    speech.lang = 'es-ES';
-    speechSynthesis.speak(speech);
+function check_text_emptying(text) {
+  if (text == "") {
+    return true;
   } else {
-    console.log("La síntesis de voz no es compatible con este navegador.");
+    return false;
   }
+}
+
+function transformValorInput(velocity) {
+  if (velocity == 5){
+    return 250;
+  }else if(velocity == 4){
+    return 625;
+  }else if(velocity == 3){
+    return 1000;
+  }else if(velocity == 2){
+    return 1500;
+  }else{
+    return 2000;
+  }
+  
+}
+
+function paint_node(node) {
+  var shape = node.findObject("SHAPE");
+  shape.fill = "#A5D7F4";
+}
+
+function unpaint_node(node) {
+  var shape = node.findObject("SHAPE");
+  shape.fill = "white";
+}
+
+function paint_link(link, speed) {
+  link.path.stroke = "#A5D7F4";
+  var shape = link.findObject("arrow");
+  shape.fill = "#7BCAF8";
+  window.setTimeout(function(){ 
+    link.path.stroke = "black";
+    shape.fill = "black";
+  },speed);
+}
+
+function check_acceptance_status(node) {
+  if(node.data.category=="accept") {
+    speak("La palabra ha sido aceptada");
+    alert("Cadena aceptada");
+    return true;
+    } else {
+    speak("La palabra ha sido rechazada");
+    alert("Cadena rechazada");
+    return false;
+    }
+}
+
+
+function speak(text) {
+if ('speechSynthesis' in window) {
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = text;
+  speech.lang = 'es-ES';
+  speechSynthesis.speak(speech);
+} else {
+  console.log("La síntesis de voz no es compatible con este navegador.");
+}
 }
