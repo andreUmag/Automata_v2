9ac5886097d1374fe5b028ec1bdf02e0c7f8a783
andreUmag <142335765+andreUmag@users.noreply.github.com>
2023-10-10 11:32:51 -0500

    Formato a codigos

--- app/static/scripts/input.js
+++ app/static/scripts/input.js
@@ -18,59 +18,59 @@ document.addEventListener("DOMContentLoaded", function () {
       }
 
       wordList.insertBefore(listItem, wordList.firstChild);
-
     } else {
       inputField.value = "";
     }
   });
 });
 
-
-function check(){
-    
-    var text = document.getElementById("input-word").value.toLowerCase();
-    var value_slider = document.getElementById("inputSlider").value;
-    var speed = transformValorInput(value_slider);
-    unpaint();
-    if (!check_text_emptying(text)){
-      if (check_text_with_regex(text)){
-        check_word(text, 0, -1, speed);
-      } else {
-        alert("La palabra solo debe contener las letras 'a' y 'b'.");
-        speak("la palabra solo debe contener las letras a y b")
-      }
+function check() {
+  var text = document.getElementById("input-word").value.toLowerCase();
+  var value_slider = document.getElementById("inputSlider").value;
+  var speed = transformValorInput(value_slider);
+  unpaint();
+  if (!check_text_emptying(text)) {
+    if (check_text_with_regex(text)) {
+      check_word(text, 0, -1, speed);
     } else {
-      alert("No se ha ingresado ninguna cadena de texto.");
-      speak("No se ha ingresado ninguna cadena de texto.")
+      alert("La palabra solo debe contener las letras 'a' y 'b'.");
+      speak("la palabra solo debe contener las letras a y b");
     }
-    
+  } else {
+    alert("No se ha ingresado ninguna cadena de texto.");
+    speak("No se ha ingresado ninguna cadena de texto.");
+  }
 }
 
 function check_word(text, index, keynode, speed) {
-  node= myDiagram.findNodeForKey(keynode);
+  node = myDiagram.findNodeForKey(keynode);
   window.setTimeout(function () {
     paint_node(node);
     window.setTimeout(function () {
-      if(index<text.length) {
+      if (index < text.length) {
         let links = node.findTreeChildrenLinks();
-        let link = links.ub._dataArray.filter(function (link) {return link.data.text == text[index] && link.fromNode == node;});
-        if(link.length == 0) {
+        let link = links.ub._dataArray.filter(function (link) {
+          return link.data.text == text[index] && link.fromNode == node;
+        });
+        if (link.length == 0) {
           unpaint(myDiagram);
           check_acceptance_status(node);
-        } else if(link[0].data.text == text[index]) {
+        } else if (link[0].data.text == text[index]) {
           unpaint_node(node);
           paint_link(link[0], speed);
-          return check_word(text, index+1, link[0].toNode.data.id, speed);
+          return check_word(text, index + 1, link[0].toNode.data.id, speed);
         }
       } else {
         check_acceptance_status(node);
       }
     }, speed);
-  },speed/2);
+  }, speed / 2);
 }
 
 function unpaint() {
-  myDiagram.model = go.Model.fromJson(document.getElementById("mySavedModel").value);
+  myDiagram.model = go.Model.fromJson(
+    document.getElementById("mySavedModel").value
+  );
 }
 
 function check_text_with_regex(text) {
@@ -90,15 +90,15 @@ function check_text_emptying(text) {
 }
 
 function transformValorInput(velocity) {
-  if (velocity == 5){
+  if (velocity == 5) {
     return 250;
-  }else if(velocity == 4){
+  } else if (velocity == 4) {
     return 625;
-  }else if(velocity == 3){
+  } else if (velocity == 3) {
     return 1000;
-  }else if(velocity == 2){
+  } else if (velocity == 2) {
     return 1500;
-  }else{
+  } else {
     return 2000;
   }
 }
@@ -117,32 +117,31 @@ function paint_link(link, speed) {
   link.path.stroke = "#A5D7F4";
   var shape = link.findObject("arrow");
   shape.fill = "#7BCAF8";
-  window.setTimeout(function(){ 
+  window.setTimeout(function () {
     link.path.stroke = "black";
     shape.fill = "black";
-  },speed);
+  }, speed);
 }
 
 function check_acceptance_status(node) {
-  if(node.data.category=="accept") {
+  if (node.data.category == "accept") {
     speak("La palabra ha sido aceptada");
     alert("Cadena aceptada");
     return true;
-    } else {
+  } else {
     speak("La palabra ha sido rechazada");
     alert("Cadena rechazada");
     return false;
-    }
+  }
 }
-
 
 function speak(text) {
-if ('speechSynthesis' in window) {
-  const speech = new SpeechSynthesisUtterance();
-  speech.text = text;
-  speech.lang = 'es-ES';
-  speechSynthesis.speak(speech);
-} else {
-  console.log("La síntesis de voz no es compatible con este navegador.");
-}
+  if ("speechSynthesis" in window) {
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = text;
+    speech.lang = "es-ES";
+    speechSynthesis.speak(speech);
+  } else {
+    console.log("La síntesis de voz no es compatible con este navegador.");
+  }
 }
