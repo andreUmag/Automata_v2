650a9e3965ccb8c752412dd48bfeac1954d4419b
andreUmag <142335765+andreUmag@users.noreply.github.com>
2023-10-10 11:02:40 -0500

    ajuste

--- app/static/scripts/input.js
+++ app/static/scripts/input.js
@@ -1,21 +1,3 @@
-// Section for the operation of the side menu
-let listelement = document.querySelectorAll(".list_button--click");
-
-listelement.forEach((listelement) => {
-  listelement.addEventListener("click", () => {
-    listelement.classList.toggle("arrow");
-
-    let height = 0;
-    let menu = listelement.nextElementSibling;
-
-    if (menu.clientHeight == "0") {
-      height = menu.scrollHeight;
-    }
-    menu.style.height = `${height}px`;
-  });
-});
-
-
 // Section for the operation of word input and displaying words in the menu list
 document.addEventListener("DOMContentLoaded", function () {
   const inputField = document.getElementById("input-word");
---
+++ app/static/scripts/menu.js
@@ -0,0 +1,17 @@
+// Section for the operation of the side menu
+let listelement = document.querySelectorAll(".list_button--click");
+
+listelement.forEach((listelement) => {
+  listelement.addEventListener("click", () => {
+    listelement.classList.toggle("arrow");
+
+    let height = 0;
+    let menu = listelement.nextElementSibling;
+
+    if (menu.clientHeight == "0") {
+      height = menu.scrollHeight;
+    }
+    menu.style.height = `${height}px`;
+  });
+});
+
